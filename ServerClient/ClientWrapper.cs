5cf8601fbb490ace75af0f61a945616dc0e01e59
Nove developer <nove1398@hotmail.com>
2020-06-13 00:15:05 -0500

    new server config

---
+++ ServerClient/ClientWrapper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using TOH.Common.ServerData;
+
+namespace ServerClient
+{
+    public sealed class ClientWrapper
+    {
+        private static readonly ClientWrapper instance = new ClientWrapper();
+        public int BufferSize = 1024;
+        public IPAddress IP = IPAddress.Loopback;
+        public int PortNumber = 8593;
+        private int Id { get; set; }
+        private ClientTcp Tcp;
+
+        public static ClientWrapper Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private ClientWrapper()
+        {
+            Tcp = new ClientTcp();
+        }
+
+        static ClientWrapper() { }
+
+
+
+
+        public void ConnectToServer()
+        {
+            Tcp.Connect();
+            
+        }
+    }
+
+
+    public class ClientTcp
+    {
+        public TcpClient Socket;
+        private NetworkStream NetStream;
+        private byte[] Buffer;
+
+
+        public void Connect()
+        {
+            Socket = new TcpClient()
+            {
+                ReceiveBufferSize = ClientWrapper.Instance.BufferSize,
+                SendBufferSize = ClientWrapper.Instance.BufferSize
+            };
+
+            Buffer = new byte[ClientWrapper.Instance.BufferSize];
+            Socket.BeginConnect(ClientWrapper.Instance.IP, ClientWrapper.Instance.PortNumber, ConnectCallback, Socket);
+        }
+
+        private void ConnectCallback(IAsyncResult result)
+        {
+            Socket.EndConnect(result);
+            if (!Socket.Connected)
+            {
+                return;
+            }
+
+            NetStream = Socket.GetStream();
+
+            NetStream.BeginRead(Buffer, 0, ClientWrapper.Instance.BufferSize, ReceivedCallback, null);
+        }
+
+        private void ReceivedCallback(IAsyncResult result)
+        {
+            try
+            {
+                int byteLength = NetStream.EndRead(result);
+                if (byteLength <= 0)
+                    return;
+
+                byte[] data = new byte[byteLength];
+                Array.Copy(Buffer, data, byteLength);
+
+                NetStream.BeginRead(Buffer, 0, ServerTcp.DataBufferSize, ReceivedCallback, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+   
+}
---
+++ TOH.Common/ServerData/ServerTcp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TOH.Common.ServerData
+{
+   public class ServerTcp
+    { 
+        public int Id { get; set; }
+        public TCP Tcp { get; set; }
+        public static int DataBufferSize = 1024;
+
+
+        public ServerTcp(int _id)
+        {
+            Id = _id;
+            Tcp = new TCP(_id);
+        }
+    }
+
+
+    public class TCP
+    {
+        public TcpClient Socket { get; set; }
+        private readonly int Id;
+        private byte[] ReceivedBuffer;
+        private NetworkStream NetStream;
+
+
+
+        public TCP(int _id)
+        {
+            Id = _id;
+        }
+
+        public void Conenct(TcpClient socket)
+        {
+            Socket = socket;
+            Socket.ReceiveBufferSize = ServerTcp.DataBufferSize;
+            Socket.SendBufferSize = ServerTcp.DataBufferSize;
+
+            NetStream = Socket.GetStream();
+            ReceivedBuffer = new byte[ServerTcp.DataBufferSize];
+
+            NetStream.BeginRead(ReceivedBuffer, 0, ServerTcp.DataBufferSize, ReceivedCallback, null);
+        }
+
+        private void ReceivedCallback(IAsyncResult result)
+        {
+            try
+            {
+                int byteLength = NetStream.EndRead(result);
+                if (byteLength <= 0)
+                    return;
+
+                byte[] data = new byte[byteLength];
+                Array.Copy(ReceivedBuffer,data, byteLength);
+
+                NetStream.BeginRead(ReceivedBuffer, 0, ServerTcp.DataBufferSize, ReceivedCallback, null);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+    }
+}
